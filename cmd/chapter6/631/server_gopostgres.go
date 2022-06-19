8c54af36a430945b17d40fb89733fe48b6c3c442
anditakaesar <anditakaesar@gmail.com>
2022-06-19 10:37:34 +0700

    finish chapter 6

---
+++ cmd/chapter6/631/server_gopostgres.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Post struct {
+	Id      int
+	Content string
+	Author  string
+}
+
+var Db *sql.DB
+
+func init() {
+	var err error
+	Db, err = sql.Open("postgres", "port=5433 host=172.21.144.1 user=postgres dbname=gwp password=anditapostgres sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Posts(limit int) (posts []Post, err error) {
+	rows, err := Db.Query("select id, content, author from posts limit $1", limit)
+	if err != nil {
+		panic(err)
+	}
+	for rows.Next() {
+		post := Post{}
+		err = rows.Scan(&post.Id, &post.Content, &post.Author)
+		if err != nil {
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
+}
+
+func GetPost(id int) (post Post, err error) {
+	post = Post{}
+	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+	return
+}
+
+func (post *Post) Create() (err error) {
+	statement := "insert into posts (content, author) values ($1, $2) returning id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
+}
+
+func (post *Post) Update() (err error) {
+	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
+	return
+}
+
+func (post *Post) Delete() (err error) {
+	_, err = Db.Exec("delete from posts where id = $1", post.Id)
+	return
+}
+
+func main() {
+	post := Post{Content: "Hellow Go!", Author: "Andita Kaesar"}
+	fmt.Println(post, "--> initial data")
+	post.Create()
+	fmt.Println(post, "--> created on Db")
+
+	readPost, _ := GetPost(post.Id)
+	fmt.Println(readPost, "--> read from db using new id")
+
+	readPost.Content = "Hello hello!"
+	readPost.Author = "Sate Manggarai"
+	readPost.Update()
+	fmt.Println(readPost, "--> after update data")
+
+	posts, _ := Posts(10)
+	fmt.Println(posts, "--> print all current data")
+
+	for _, p := range posts {
+		p.Delete()
+	}
+}
